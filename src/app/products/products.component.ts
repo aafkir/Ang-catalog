b9ae320380bf51ec3c5ffabde3b7cc7d54a85d72
aafkir <a.afkkir@gmail.com>
2022-11-03 22:00:16 +0100

    config bootstrap/js, routing and first comp

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+    products! : Array <any>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  this.products = [
+  {id:1, name :"computer", price : 100},
+  {id:2, name :"computer3", price : 200},
+  {id:3, name :"computer4", price : 300},
+  {id:4, name :"computer5", price : 400},
+
+
+  ]
+  }
+
+}
